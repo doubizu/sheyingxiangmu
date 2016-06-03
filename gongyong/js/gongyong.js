f5cb348a29f50b521cde4abf3687b9315c33f71f
liulanger <1403710342@qq.com>
2016-06-03 21:38:23 +0800

    页头页脚，不过轮播图那里的文字还有一点问题

---
+++ gongyong/js/gongyong.js
@@ -0,0 +1,25 @@
+$(function() {
+	$("#header-right").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false,
+		infinite: true,
+		pauseOnHover: false,
+		pauseOnFocus: false
+	});
+	$("#your-class").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false,
+		infinite: true,
+		pauseOnHover: false,
+		pauseOnFocus: false
+	});
+	
+});
