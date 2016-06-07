c522f57f2119bdd092d27f2c9d3861019a9efd83
liulanger <1403710342@qq.com>
2016-06-08 01:19:38 +0800

    首页的大概页面

---
+++ xiangmu/js/gongyong.js
@@ -0,0 +1,14 @@
+$(function() {
+	$("#header-right").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false,
+		infinite: true,
+		pauseOnHover: false,
+		pauseOnFocus: false
+	});
+	
+});
